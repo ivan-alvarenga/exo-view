9bcfc24bfe810a30f1cda1f83ac9400ab36b9f58
ivan.alvarenga <ivan.neun@gmail.com>
2019-11-13 10:35:04 -0300

    adiçao de componentes de pesquisa

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    
+    <div class="md-layout">
+      <div class="md-layout-item md-size-30 md-alignment-left-center">
+        <label>Planeta</label>
+        <div><md-checkbox v-model="array" value="1">Nome</md-checkbox></div>        
+        <div><md-checkbox v-model="array" value="1">Massa</md-checkbox></div>        
+        <div><md-checkbox v-model="array" value="1">Massa Relativa(planeta/terra)</md-checkbox></div>        
+        <div><md-checkbox v-model="array" value="1">Massa Relativa(planeta/júpiter)</md-checkbox></div>        
+      </div>
+      <div class="md-layout-item md-size-50">
+        <md-field>
+          <label>Pesquisar</label>
+          <md-input v-model="initial"></md-input>
+          <md-button>Nome do planeta</md-button>
+        </md-field>
+        <md-field>          
+          <md-input v-model="initial"></md-input>
+          <md-button>Nome do planeta</md-button>
+        </md-field>
+        <Home parentMessage="Bem-vindo ao explorador de exoplanetas" :headers="heads" :values="values"/>
+      </div>
+      <div class="md-layout-item"></div>
+  </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Home
+  },
+  data() {
+    return {
+      heads: [
+        {
+          id: 1,
+          name: 'coluna 1',
+        },
+        {
+          id: 2,
+          name: 'coluna 2',          
+        },
+        {
+          id: 3,
+          name: 'coluna 3',
+        },
+      ],
+      values: [
+        { id: 1, nome: 'joão', idade: 23},
+        { id: 2, nome: 'josé', idade: 32},
+        { id: 3, nome: 'alberto', idade: 43},
+      ],
+    }
+  },
 }
+
 </script>
 
 <style>
---
+++ src/components/Home/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <md-table>
+      <md-table-toolbar>        
+        <h1 class="md-title">{{ parentMessage }}</h1>        
+      </md-table-toolbar>
+        
+      <md-table-row>
+        <md-table-head v-for="head in headers" :key="head.id">{{ head.name }}</md-table-head>        
+      </md-table-row> 
+
+      <md-table-row v-for="val in values" :key="val.id">
+        <md-table-cell md-numeric>{{val.id}}</md-table-cell>
+        <md-table-cell>{{val.nome}}</md-table-cell>
+        <md-table-cell md-numeric>{{val.idade}}</md-table-cell>        
+      </md-table-row>
+      
+    </md-table>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    parentMessage: String,
+    headers: Object,
+    values: Object,
+  }
+}
+</script>
